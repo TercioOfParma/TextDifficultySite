27dc2f38569dd045c8e0ea2e4120d51d614365e8
Elliot Lake <gigaraptorama@gmail.com>
2023-11-10 14:57:20 +0000

    Create DbContext

---
+++ TextDifficultyDeterminer.Infrastructure/Database/TextDifficultyDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+
+public class TextDifficultyDbContext : DbContext 
+{
+    public DbSet<FrequencyWord> Words {get; set;}
+    public string DbPath {get; set;} = "TextCorpus.db";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
