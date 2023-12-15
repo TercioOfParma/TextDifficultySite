bb33e393a65970f29bb923a87cd366a7b04e7205
Elliot Lake <gigaraptorama@gmail.com>
2023-12-15 13:39:15 +0000

    Replicate Change from API to Internal to All Input Screens

--- TextDifficultyDeterminer.Website/Pages/TestCorpus/TestCorpusBase.cs
+++ TextDifficultyDeterminer.Website/Pages/TestCorpus/TestCorpusBase.cs
@@ -1,12 +1,14 @@
 using TextDifficultyDeterminer.Website.Shared;
+using TextDifficultyDeterminer.Website.Services;
 using TextDifficultyDeterminer.Website.Dtos;
-using Microsoft.AspNetCore.Components.Forms;
 using Radzen.Blazor;
 using Radzen;
 using Microsoft.JSInterop;
 using System.Text.Json;
 using Microsoft.AspNetCore.Components;
 using ClosedXML.Excel;
+using MediatR;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace TextDifficultyDeterminer.Website.TestCorpus 
 {
@@ -14,15 +16,30 @@ namespace TextDifficultyDeterminer.Website.TestCorpus
     {
         [Inject]
         public IJSRuntime JS {get; set;}
+        [Inject]
+        protected ProcessTextFiles TextFiles {get; set;}
         public TestCorpusUploadDto TestCorpusUploadDto {get; set; } = new();
         public bool IsNotZip {get; set;} = false;
         public RadzenUpload Upload {get;set;} = new();
+        public IReadOnlyList<IBrowserFile> FilesToUpload {get; set;}
+        private const int MAX_TEXT_FILES = 1000;
 
-        public async Task HandleDownloadExcel(UploadCompleteEventArgs e)
+        public async Task UpdateFiles(InputFileChangeEventArgs e) =>
+            FilesToUpload = e.GetMultipleFiles(MAX_TEXT_FILES);
+        public async Task HandleCorpus()
         {
-            Console.WriteLine($"Handling Response! {e.JsonResponse.ToString()}");
-            var container = JsonSerializer.Deserialize<TextContainer>(e.RawResponse);
-            Console.WriteLine($"Deserialised!");
+            var fileList = FilesToUpload;
+            Dictionary<string, string> dict = new();
+            foreach(var file in fileList)
+            {
+                if(file.ContentType != "text/plain")
+                    continue;
+                var reader = new StreamReader(file.OpenReadStream());
+                var textForFile = await reader.ReadToEndAsync();
+                dict[file.Name] = textForFile;
+            }
+
+            var container = await TextFiles.LoadFiles(dict);
             var excelFile = await Mediator.Send(new TextContainerToExcelCommand { Container = container});
             
             var stream = new MemoryStream();
--- TextDifficultyDeterminer.Website/Pages/UploadToDatabase/UploadTextBoxBase.cs
+++ TextDifficultyDeterminer.Website/Pages/UploadToDatabase/UploadTextBoxBase.cs
@@ -1,27 +1,50 @@
 using TextDifficultyDeterminer.Website.Shared;
+using TextDifficultyDeterminer.Website.Services;
 using TextDifficultyDeterminer.Website.Dtos;
 using Radzen.Blazor;
 using Radzen;
+using Microsoft.JSInterop;
+using System.Text.Json;
+using Microsoft.AspNetCore.Components;
+using ClosedXML.Excel;
+using MediatR;
+using Microsoft.AspNetCore.Components.Forms;
+
 namespace TextDifficultyDeterminer.Website.UploadToDatabase 
 {
     public class UploadTextBoxBase : AppComponent 
     {
         public TestCorpusUploadDto TestCorpusUploadDto {get; set; } = new();
+        [Inject]
+        protected ProcessTextFiles TextFiles {get; set;}
         public Guid LanguageId {get; set;}
         public List<Language> LanguageList {get; set;}
         public RadzenUpload Upload {get;set;} = new();
         public bool IsUploaded {get; set;} = false;
-
+        public IReadOnlyList<IBrowserFile> FilesToUpload {get; set;}
+        private const int MAX_TEXT_FILES = 1000;
         protected async override Task OnInitializedAsync()
         {
             var languages = await Mediator.Send(new GetLanguagesQuery {} );
             LanguageList = languages.LanguageList;
         }
-
-        protected async Task HandleResponseUploadToDatabase(UploadCompleteEventArgs e)
+        public async Task UpdateFiles(InputFileChangeEventArgs e) =>
+            FilesToUpload = e.GetMultipleFiles(MAX_TEXT_FILES);
+        protected async Task HandleCorpus()
         {
-            Console.WriteLine("DONE!");
-            IsUploaded = true;
+            var fileList = FilesToUpload;
+            Dictionary<string, string> dict = new();
+            foreach(var file in fileList)
+            {
+                if(file.ContentType != "text/plain")
+                    continue;
+                var reader = new StreamReader(file.OpenReadStream());
+                var textForFile = await reader.ReadToEndAsync();
+                dict[file.Name] = textForFile;
+            }
+
+            await TextFiles.LoadFilesIntoDatabase(LanguageId, dict);
         }
+
     }
 }
