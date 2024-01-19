59cde47493ad727ae88724e73455c44c0fd3895e
Elliot Lake <gigaraptorama@gmail.com>
2024-01-19 18:01:33 +0000

    Begin HTMXFrontend Display

---
+++ TextDifficultyDeterminer.HTMXFrontend/Controllers/HTMXFrontendController.cs
@@ -0,0 +1,13 @@
+import Microsoft.AspNetCore.Mvc;
+
+using TextDifficultyDeterminer.Website.Services;
+
+namespace TextDifficultyDeterminer.HTMXFrontend.Controllers 
+{
+[ApiController]
+[Microsoft.AspNetCore.Mvc.Route("api/")]
+public class HTMXFrontendController : ControllerBase
+{
+    [HttpGet]
+}
+}
